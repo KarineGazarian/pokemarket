3d3f995a78b2ef6959780d24258b31ffdee9dcc9
alice de chalendar <alice.dechalendar@gmail.com>
2019-05-20 17:12:01 +0100

    created Pokemon model and views, plus user and pokemon seeds

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,31 @@
+class PokemonsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @pokemons = Pokemon.all
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @pokemon = Pokemon.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @pokemon = pokemon.new(poke_params)
+    @pokemon.user = @user
+    @pokemon.save
+
+    redirect_to root_path
+  end
+
+  def show
+    @pokemon = Pokemon.find(params[:id])
+  end
+
+  private
+
+  def poke_params
+    params.require(:pokemon).permit(:name, :category, :level, :price)
+  end
+end
