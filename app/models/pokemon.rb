f6e65f7f5f01ee88be75c859b31a3fb8fd701f29
Frankie Ling <ingleson.fl@gmail.com>
2019-05-22 14:17:32 +0100

    adding a pokemon to sell form

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -2,6 +2,9 @@ class Pokemon < ApplicationRecord
   belongs_to :user
 
   validates :name, :price, presence: true
+  validates :category, presence: true, inclusion: { in: %w(normal fire water grass electric ice fighting poison ground flying psychic bug rock ghost dark dragon steel fairy),
+     message: "%{value} is not a valid category"
+  }
 
   mount_uploader :photo, PhotoUploader
 end
