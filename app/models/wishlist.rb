04acbf47ab38f8a0c5743921da304a2e16abba90
vladimir deziegler <vladimirdeziegler@gmail.com>
2019-05-22 16:29:27 +0100

    button modified

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,9 +1,6 @@
 class Wishlist < ApplicationRecord
   belongs_to :user
   belongs_to :pokemon
-<<<<<<< HEAD
-=======
 
   validates :pokemon, uniqueness: true
->>>>>>> c87f1168db25adc45445fd850ead8d0993ef3e8b
 end
