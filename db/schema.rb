a0581ffc62ced7bad014f07ec5733ff7f2a3cd78
Frankie Ling <ingleson.fl@gmail.com>
2019-05-23 12:04:10 +0100

    added schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_105159) do
+ActiveRecord::Schema.define(version: 2019_05_23_095149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
+  end
 
   create_table "pokemons", force: :cascade do |t|
     t.string "name"
