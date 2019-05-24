35c3b44c9d6814d5249832c9e1da078298f2d3b1
vladimir deziegler <vladimirdeziegler@gmail.com>
2019-05-24 14:38:42 +0100

    song added to home

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,24 @@
 import "bootstrap";
+
+
+if (document.querySelector(".pages.home")) {
+  // var audio = new Audio('../songs/Pokemon.mp3');
+  // audio.play();
+    var audio = new Audio;
+
+    function playSound() {
+      audio.src = ('/audios/Pokemon.mp3');
+      audio.play()
+    }
+
+
+   window.onload = playSound();
+};
+
+
+
+
+
+// ''
+
+
