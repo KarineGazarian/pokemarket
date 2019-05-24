9e0b10cb22d5f20cf79af4732cd6262e81588133
alice de chalendar <alice.dechalendar@gmail.com>
2019-05-24 15:21:52 +0100

    fixed sound overlaps

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,19 +6,15 @@ import { pokebowl } from "./pokebowl";
 if (document.querySelector(".pages.home")) {
   // var audio = new Audio('../songs/Pokemon.mp3');
   // audio.play();
-    var audio = new Audio;
+   //  var audio = new Audio;
 
-    function playSound() {
-      audio.src = ('/audios/Pokemon.mp3');
-      audio.play()
-    }
+   //  function playSound() {
+   //    audio.src = ('/audios/Pokemon.mp3');
+   //    audio.play()
+   //  }
 
 
-   window.onload = playSound();
+   // window.onload = playSound();
 };
 
 
-
-
-
-// ''
